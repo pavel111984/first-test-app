e8411394666ff5fa5bb99f1cd808219d85c4b816
pavel111984 <pavel.mikhaylov84@gmail.com>
2018-08-16 22:02:43 +0300

    lesson 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.materialize.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My Angular Application';
+  
+  users = [
+  {name: 'WFM 1'},
+  {name: 'WFM 2'},
+  {name: 'WFM 3'},
+  {name: 'WFM 4'},
+  {name: 'WFM 5'},
+  {name: 'WFM 6'},
+  {name: 'WFM 7'},
+  {name: 'WFM 8'}
+  
+  ]
 }
